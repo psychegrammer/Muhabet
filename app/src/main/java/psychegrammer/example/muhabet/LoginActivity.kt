4c9690f068c49042f424457318c756fcadef2c23
psychegrammer <psychegrammer@gmail.com>
2019-07-27 15:09:47 +0200

    Initial commit

---
+++ app/src/main/java/psychegrammer/example/muhabet/LoginActivity.kt
@@ -0,0 +1,37 @@
+package psychegrammer.example.muhabet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login", "Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener() {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    // else if successful
+                    Log.d("MainActivity", "User with uid: ${it.result?.user?.uid} has successfully logged in.")
+                }
+                .addOnFailureListener() {
+                    Log.d("MainActivity", "User has failed to login: ${it.message}")
+                }
+        }
+
+        back_to_register_textview.setOnClickListener {
+            finish()
+        }
+    }
+}
